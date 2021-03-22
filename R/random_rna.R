77bcf313bced5775e73135ec840295cf8f0cf26b
Mikkel Eriksen <m.spallou@gmail.com>
2021-03-22 08:58:40 +0000

    Random RNA function

---
+++ R/random_rna.R
@@ -0,0 +1,15 @@
+#' Get a random RNA sequence
+#' 
+#' @param l Length of RNA sequence
+#' 
+#' @return rna String of random RNA sequence
+#' 
+#' @example 
+#' random_rna(30)
+#' 
+#' @export
+random_rna <- function(l){
+  nucleotides <- sample(c("A", "U", "G", "C"), size = l, replace = TRUE)
+  rna = paste0(nucleotides, collapse = "")
+  return(rna)
+}
